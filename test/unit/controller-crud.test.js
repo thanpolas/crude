53cae18c9394691b93ddf5fc2ba17f87be737c5b
Thanasis Polychronakis <thanpolas@gmail.com>
2013-06-26 11:49:00 +0300

    update packages

---
+++ test/unit/controller-crud.test.js
@@ -0,0 +1,32 @@
+/**
+ * @fileOverview Testing the model validators.
+ */
+
+// var sinon  = require('sinon');
+var chai = require('chai');
+var assert = require('chai').assert;
+
+// module to test
+var ctrlCrud = require('../../controller-crud');
+
+// var noop = function(){};
+
+suite('10. Controller CRUD', function() {
+
+  setup(function() {});
+  teardown(function() {});
+
+
+  // The numbering (e.g. 1.1.1) has nothing to do with order
+  // The purpose is to provide a unique string so specific tests are
+  // run by using the mocha --grep "1.1.1" option.
+
+  suite('10.1 Helper funcs', function() {
+    var source;
+    setup(function() {});
+
+    test('10.1.1 process', function() {
+
+    });
+  });
+});
