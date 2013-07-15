fe0849795325b1550891e97448254f5d77d52fe4
Thanasis Polychronakis <thanpolas@gmail.com>
2013-07-15 15:27:48 +0300

    update entity base class

--- entity.js
+++ entity.js
@@ -2,6 +2,8 @@
  * @fileOverview The Entity base class.
  */
 
+var midd = require('middlewarify');
+
 /**
  * The base Entity Class all entities extend from.
  *
@@ -15,6 +17,15 @@ var Entity = module.exports = function(Model, optUdo) {
 
   /** @type {?Object} The current user or null for anonymous */
   this.udo = optUdo || null;
+
+  // Create primitive middlewares
+  midd.make(this, 'create', this._create);
+  midd.make(this, 'read', this._read);
+  midd.make(this, 'readOne', this._readOne);
+  midd.make(this, 'readLimit', this._readLimit);
+  midd.make(this, 'update', this._update);
+  midd.make(this, 'delete  ', this._delete  );
+  midd.make(this, 'count', this._count);
 };
 
 /**
@@ -30,8 +41,9 @@ Entity.prototype.setUdo = function(udo) {
  *
  * @param {Object} itemData The data to use for creating.
  * @param {Function(ts.error.Abstract=, mongoose.Document=)} done callback.
+ * @protected
  */
-Entity.prototype.create = function(itemData, done) {
+Entity.prototype._create = function(itemData, done) {
   throw new Error('Not Implemented');
 };
 
@@ -40,8 +52,9 @@ Entity.prototype.create = function(itemData, done) {
  *
  * @param {string|Object} id the item id or an Object to query against.
  * @param {Function(ts.error.Abstract=, mongoose.Document=)} done callback.
+ * @protected
  */
-Entity.prototype.readOne = function(id, done) {
+Entity.prototype._readOne = function(id, done) {
   throw new Error('Not Implemented');
 };
 
@@ -51,8 +64,9 @@ Entity.prototype.readOne = function(id, done) {
  *
  * @param {Object|string=} optQuery Optionally define a query to limit results.
  * @param {Function(ts.error.Abstract=, mongoose.Document=)} done callback.
+ * @protected
  */
-Entity.prototype.read = function(optQuery, done) {
+Entity.prototype._read = function(optQuery, done) {
   throw new Error('Not Implemented');
 };
 
@@ -63,8 +77,9 @@ Entity.prototype.read = function(optQuery, done) {
  * @param {number} skip starting position.
  * @param {number} limit how many records to fetch.
  * @param {Function(ts.error.Abstract=, Array.<mongoose.Document>=)} done callback.
+ * @protected
  */
-Entity.prototype.readLimit = function(query, skip, limit, done) {
+Entity.prototype._readLimit = function(query, skip, limit, done) {
   throw new Error('Not Implemented');
 };
 
@@ -73,8 +88,9 @@ Entity.prototype.readLimit = function(query, skip, limit, done) {
  *
  * @param {?Object} query Narrow down the set, set to null for all.
  * @param {Function(ts.error.Abstract=, number=)} done callback.
+ * @protected
  */
-Entity.prototype.count = function(query, done) {
+Entity.prototype._count = function(query, done) {
   throw new Error('Not Implemented');
 };
 
@@ -84,8 +100,9 @@ Entity.prototype.count = function(query, done) {
  * @param {string} id the item id.
  * @param {Object} itemData The data to use for creating.
  * @param {Function(ts.error.Abstract=, mongoose.Document=)} done callback.
+ * @protected
  */
-Entity.prototype.update = function(id, itemData, done) {
+Entity.prototype._update = function(id, itemData, done) {
   throw new Error('Not Implemented');
 };
 
@@ -94,8 +111,9 @@ Entity.prototype.update = function(id, itemData, done) {
  *
  * @param {string} id the item id.
  * @param {Function(ts.error.Abstract=, mongoose.Document=)} done callback.
+ * @protected
  */
-Entity.prototype.delete = function(id, done) {
+Entity.prototype._delete = function(id, done) {
   throw new Error('Not Implemented');
 };
 
