4fdc579d7afb7534e2c1074534d4fbc743eb3404
Thanasis Polychronakis <thanpolas@gmail.com>
2013-07-15 18:22:42 +0300

    adding more entity tests...

--- test/unit/entity.test.js
+++ test/unit/entity.test.js
@@ -4,6 +4,7 @@
 
 // var sinon  = require('sinon');
 var chai = require('chai');
+var sinon = require('sinon');
 var assert = chai.assert;
 
 // module to test
@@ -33,8 +34,59 @@ suite('2. Entity', function() {
       assert.isFunction(ent.readLimit, 'Entity should have a "readLimit" method');
       assert.isFunction(ent.update, 'Entity should have a "update" method');
       assert.isFunction(ent.delete, 'Entity should have a "delete" method');
-      assert.isFunction(ent.setUdo, 'Entity should have a "setUdo" method');
       assert.isFunction(ent.count, 'Entity should have a "count" method');
+    });
+    test('2.1.2 Helper Methods', function(){
+      assert.isFunction(ent.setUdo, 'Entity should have a "setUdo" method');
+    });
+    test('2.1.3 Primitive Methods middleware "use" method', function(){
+      assert.isFunction(ent.create.use, 'Entity should have a "create.use" method');
+      assert.isFunction(ent.read.use, 'Entity should have a "read.use" method');
+      assert.isFunction(ent.readOne.use, 'Entity should have a "readOne.use" method');
+      assert.isFunction(ent.readLimit.use, 'Entity should have a "readLimit.use" method');
+      assert.isFunction(ent.update.use, 'Entity should have a "update.use" method');
+      assert.isFunction(ent.delete.use, 'Entity should have a "delete.use" method');
+      assert.isFunction(ent.count.use, 'Entity should have a "count.use" method');
+    });
+
+
+    test('2.1.4 Primitive methods are not implemented', function(){
+      var spyCreate = sinon.spy();
+      var spyRead = sinon.spy();
+      var spyReadOne = sinon.spy();
+      var spyReadLimit = sinon.spy();
+      var spyUpdate = sinon.spy();
+      var spyDelete = sinon.spy();
+      var spyCount = sinon.spy();
+
+      ent.create(spyCreate);
+      ent.read(spyRead);
+      ent.readOne(spyReadOne);
+      ent.readLimit(spyReadLimit);
+      ent.update(spyUpdate);
+      ent.delete(spyDelete);
+      ent.count(spyCount);
+
+      console.log('create:', spyCreate.args[0]);
+
+      assert.instanceOf(spyCreate.args[0], Error, 'first arg should be instanceOf Error');
+      assert.instanceOf(spyRead.args[0], Error, 'first arg should be instanceOf Error');
+      assert.instanceOf(spyReadOne.args[0], Error, 'first arg should be instanceOf Error');
+      assert.instanceOf(spyReadLimit.args[0], Error, 'first arg should be instanceOf Error');
+      assert.instanceOf(spyUpdate.args[0], Error, 'first arg should be instanceOf Error');
+      assert.instanceOf(spyDelete.args[0], Error, 'first arg should be instanceOf Error');
+      assert.instanceOf(spyCount.args[0], Error, 'first arg should be instanceOf Error');
+
+      var message = 'Not Implelemted';
+      assert.equal(message, spyCreate.args[0], 'Error Message should be "Not Implemented"');
+      assert.equal(message, spyRead.args[0], 'Error Message should be "Not Implemented"');
+      assert.equal(message, spyReadOne.args[0], 'Error Message should be "Not Implemented"');
+      assert.equal(message, spyReadLimit.args[0], 'Error Message should be "Not Implemented"');
+      assert.equal(message, spyUpdate.args[0], 'Error Message should be "Not Implemented"');
+      assert.equal(message, spyDelete.args[0], 'Error Message should be "Not Implemented"');
+      assert.equal(message, spyCount.args[0], 'Error Message should be "Not Implemented"');
+
+
     });
   });
 });
