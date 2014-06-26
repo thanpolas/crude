8072c2de4385d1a67798d6a6f1aba5f69461ea64
Thanasis Polychronakis <thanpolas@gmail.com>
2014-06-26 09:25:32 +0300

    create user entity test setup

---
+++ test/setup/user.ent.js
@@ -0,0 +1,49 @@
+/**
+ * @fileOverview Users entity.
+ */
+
+var EntityCrudMongoose = require('node-entity').Mongoose;
+
+var UserModel = require('.user.mongoose.model');
+
+var userModel = new UserModel();
+
+/**
+ * The User entity.
+ *
+ * @constructor
+ * @extends {Entity.Mongoose}
+ */
+var User = module.exports = EntityCrudMongoose.extendSingleton(function() {
+  this.setModel(userModel.Model);
+});
+
+/**
+ * Create a UDO for public use by the UDO owner.
+ *
+ * @param  {Mongoose.Model} udo The raw udo.
+ * @return {Object} A safe to use UDO.
+ * @static
+ */
+User.secureOwnUdo = function(udoDocument) {
+  var udo = Object.create(null);
+  udo.id = udoDocument.id;
+  udo.email = udoDocument.email;
+  udo.name = udoDocument.name;
+  udo.isVerified = udoDocument.isVerified;
+  return udo;
+};
+
+/**
+ * Create a UDO for public use by other users.
+ *
+ * @param  {Mongoose.Model} udo The raw udo.
+ * @return {Object} A safe to use UDO.
+ * @static
+ */
+User.securePublicUdo = function(udoDocument) {
+  var udo = User.secureOwnUdo(udoDocument);
+  delete udo.email;
+  delete udo.isVerified;
+  return udo;
+};
