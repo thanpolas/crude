6b7d990f923965d609fd90a5845ffffb5711e77e
Thanasis Polychronakis <thanpolas@gmail.com>
2014-07-01 17:50:14 +0300

    properly expose Test ctor and test for proper exposing

--- index.js
+++ index.js
@@ -5,3 +5,4 @@
 var crude = module.exports = require('./lib/controller-crud-export');
 
 crude.route = require('./lib/route');
+crude.Test = require('./test/export/generic-crud-test.lib');
--- test/integration/api-test-helper.test.js
+++ test/integration/api-test-helper.test.js
@@ -1,19 +1,25 @@
 /**
  * @fileOverview Test the exposed Test helpers API.
  */
+var chai = require('chai');
+var expect = chai.expect;
 
 var UserEnt = require('../setup/user.ent');
 
 var tester = require('../lib/tester.lib');
 
-var TestCrud = require('../export/generic-crud-test.lib');
+var crude = require('../..');
 var userFix = require('../fixtures/user.fix');
 
 describe('Test the exposed API Test Helper', function () {
   this.timeout(5000);
+  it('should expose the tester', function() {
+    expect(crude.Test).to.be.a('function');
+  });
+
   it('should execute...', function (done) {
     tester.initActual().then(function () {
-      var testCrud = new TestCrud({
+      var testCrud = new crude.Test({
         endpoint: '/user',
         fixture: userFix.one,
         stringAttr: 'firstName',
