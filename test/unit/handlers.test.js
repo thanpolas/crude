92d0cdf6250b51e3e0b02e14eaa60b38dea27349
Thanasis Polychronakis <thanpolas@gmail.com>
2014-09-01 20:43:51 +0300

    expand handlers tests

--- test/unit/handlers.test.js
+++ test/unit/handlers.test.js
@@ -18,26 +18,32 @@ describe('Error and Success Handlers', function () {
 
   describe('Error Handler', function () {
     beforeEach(function () {
-      this.error = new Error('yum');
-      this.ctrl.read.throws(this.error);
+      this.err = 'yum';
+      this.ctrl.readLimit.throws(this.err);
     });
 
     it('should call the built-in error handler', function () {
-      var handleStub = sinon.stub(this.crude, 'handleError');
-      this.crude.read(this.reqres.req, this.reqres.res);
-      expect(handleStub).to.have.been.calledOnce;
-      expect(handleStub).to.have.been.calledWith(this.reqres.req, this.reqres.res,
-        'read', 500, this.error);
-      handleStub.restore();
+      var handleStub = sinon.stub(this.crude.opts, 'onError');
+      return this.crude.readList(this.reqres.req, this.reqres.res)
+        .bind(this)
+        .then(function() {
+          expect(handleStub).to.have.been.calledOnce;
+          expect(handleStub.args[0][0]).to.equal(this.reqres.req);
+          expect(handleStub.args[0][1]).to.equal(this.reqres.res);
+          expect(handleStub.args[0][2]).to.equal('paginate');
+          expect(handleStub.args[0][3]).to.equal(500);
+          expect(handleStub.args[0][4]).to.equal(this.err);
+          handleStub.restore();
+        });
     });
 
     it('should call the custom error handler', function () {
       this.spy = sinon.spy();
       this.crude.onError(this.spy);
-      this.crude.read(this.reqres.req, this.reqres.res);
+      this.crude.readList(this.reqres.req, this.reqres.res);
       expect(this.spy).to.have.been.calledOnce;
       expect(this.spy).to.have.been.calledWith(this.reqres.req, this.reqres.res,
-        'read', 500, this.error);
+        'read', 500, this.err);
     });
   });
 });
