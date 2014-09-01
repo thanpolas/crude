77fcb8fc0ad81021ea8de09b428e04669787f572
Thanasis Polychronakis <thanpolas@gmail.com>
2014-09-01 21:56:56 +0300

    Expand handler tests for custom error handlers

--- test/unit/handlers.test.js
+++ test/unit/handlers.test.js
@@ -16,14 +16,14 @@ describe('Error and Success Handlers', function () {
     this.reqres = tester.reqres();
   });
 
-  describe('Error Handler', function () {
+  describe('Error Handlers', function () {
     beforeEach(function () {
       this.err = new Error('yum');
     });
     afterEach(function () {
     });
 
-    describe('Built-in Handler', function () {
+    describe('Built-in Error Handler', function () {
       beforeEach(function () {
         this.handleStub = sinon.stub(this.crude.opts, 'onError');
       });
@@ -89,16 +89,67 @@ describe('Error and Success Handlers', function () {
 
     });
 
-    it('should call the custom error handler', function (done) {
-      this.spy = sinon.spy();
-      this.crude.onError(this.spy);
-      return this.crude.readList(this.reqres.req, this.reqres.res)
-        .bind(this)
-        .then(function() {
+    describe('Custom Error Handler', function () {
+      function runAssert (operation) {
+        return function () {
           expect(this.spy).to.have.been.calledOnce;
           expect(this.spy).to.have.been.calledWith(this.reqres.req, this.reqres.res,
-            'paginate', 500, this.err);
-        }).then(done, done);
+            operation, 500, this.err);
+        };
+      }
+      beforeEach(function () {
+        this.spy = sinon.spy();
+        this.crude.onError(this.spy);
+      });
+
+
+      it('should work on pagination', function (done) {
+        this.ctrl.readLimit.throws(this.err);
+        return this.crude.readList(this.reqres.req, this.reqres.res)
+          .bind(this)
+          .then(runAssert('paginate'))
+          .then(done, done);
+
+      });
+      it('should work on read all', function (done) {
+        this.ctrl.read.throws(this.err);
+        this.crude.config({pagination: false});
+        return this.crude.readList(this.reqres.req, this.reqres.res)
+          .bind(this)
+          .then(runAssert('read'))
+          .then(done, done);
+      });
+      it('should work on read one', function (done) {
+        this.ctrl.readOne.throws(this.err);
+        this.reqres.req.params.id = 'one';
+        return this.crude.readOne(this.reqres.req, this.reqres.res)
+          .bind(this)
+          .then(runAssert('readOne'))
+          .then(done, done);
+      });
+      it('should work on update', function (done) {
+        this.ctrl.readOne.throws(this.err);
+        this.reqres.req.params.id = 'one';
+        return this.crude.update(this.reqres.req, this.reqres.res)
+          .bind(this)
+          .then(runAssert('update'))
+          .then(done, done);
+      });
+      it('should work on delete', function (done) {
+        this.ctrl.delete.throws(this.err);
+        this.reqres.req.params.id = 'one';
+        return this.crude.delete(this.reqres.req, this.reqres.res)
+          .bind(this)
+          .then(runAssert('delete'))
+          .then(done, done);
+      });
+      it('should work on create', function (done) {
+        this.ctrl.create.throws(this.err);
+        return this.crude.create(this.reqres.req, this.reqres.res)
+          .bind(this)
+          .then(runAssert('create'))
+          .then(done, done);
+      });
     });
   });
 });
