9a434efd2066d7fae515f25f278dbd8e48c59d13
Thanasis Polychronakis <thanpolas@gmail.com>
2014-09-06 14:13:10 +0300

    work on e2e read test

--- test/e2e/read.test.js
+++ test/e2e/read.test.js
@@ -4,21 +4,37 @@
 var chai = require('chai');
 var expect = chai.expect;
 
-var testlib = require('crude-test-case');
-testlib.setCrude(require('../../'));
+var crude = require('../..');
 
-var setupUsers = testlib.libUser;
-var tester = testlib.tester;
+var testCase = require('crude-test-case');
+testCase.setCrude(crude);
+var Web = testCase.Web;
+
+var testerLocal = require('../lib/tester.lib');
 
 describe('Read OPs', function() {
   this.timeout(5000);
 
-  setupUsers.createUser();
+  beforeEach(function (done) {
+    testCase.expressApp.init()
+      .then(done, done);
+  });
 
-  describe('Read records', function () {
+  beforeEach(function() {
+    var web = new Web();
+    this.req = web.req;
+  });
+
+  beforeEach(function () {
+    // Setup crude
+    this.ctrl = testerLocal.controller();
+    this.crude = crude('/mock', this.ctrl, testCase.expressApp.app);
+  });
+
+  describe.only('Read records', function () {
     beforeEach(function(done) {
       var self = this;
-      this.req.get('/user')
+      this.req.get('/mock')
         .expect(200)
         .end(function(err, res) {
           if (err) {
@@ -31,34 +47,24 @@ describe('Read OPs', function() {
           done();
         });
     });
-    it('Should have proper length', function() {
-      expect(this.body).to.have.length(3);
+
+    it('Should have proper type and length', function() {
+      expect(this.body).to.be.an('array');
+      expect(this.body).to.have.length(1);
     });
     it('Should have proper keys', function () {
       expect(this.body[0]).to.have.keys([
-        '_id',
-        'birthdate',
-        'firstName',
-        'lastName',
-        'companyName',
-        'email',
-        'password',
-        'createdOn',
-        'isVerified',
-        'isDisabled',
-        'isAdmin',
+        'a',
       ]);
     });
     it('Should have proper values', function () {
-      expect(this.body[0].firstName).to.equal('John');
-      expect(this.body[0].lastName).to.equal('Doe');
-      expect(this.body[0].companyName).to.equal('');
-      expect(this.body[0].email).to.equal('pleasant@hq.com');
-      expect(this.body[0].password).to.equal('123456');
-      expect(this.body[0].createdOn).to.match(tester.reIso8601);
-      expect(this.body[0].isVerified).to.equal(true);
-      expect(this.body[0].isDisabled).to.equal(false);
-      expect(this.body[0].isAdmin).to.equal(false);
+      expect(this.body[0].a).to.equal(1);
+    });
+    it('Should invoke ctrl readLimit', function () {
+      expect(this.ctrl.readLimit).to.have.been.calledOnce;
+    });
+    it('Should invoke ctrl readLimit with expected args', function () {
+      expect(this.ctrl.readLimit).to.have.been.calledWith();
     });
   });
 
